7506311a58c8674ff1aa4b9d51bf24706a135cb2
TradeRabbit <68387028+TradeRabbit@users.noreply.github.com>
2022-09-19 13:29:28 -0300

    Teste

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ csrf = CSRFProtect(app)
 
 @app.route("/")
 def pagina_inicial():
-    return "Laboratório DevOps - FIAP 8ASO"
+    return "Laboratório DevOps - FIAP 8ASO - Luciano Oliveira"
 
 if __name__ == '__main__':
     port = os.getenv('PORT')
