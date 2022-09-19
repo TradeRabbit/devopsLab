e752f81013f20b47071c6cd972610f41ed5689ae
TradeRabbit <68387028+TradeRabbit@users.noreply.github.com>
2022-09-19 13:57:38 -0300

    Teste Grupo 21

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ csrf = CSRFProtect(app)
 
 @app.route("/")
 def pagina_inicial():
-    return "Laboratório DevOps - FIAP 8ASO - Grupo 21"
+    return "Laboratório DevOps - FIAP 8ASO - Grupo 21 - Luciano Oliveira, Leandro Gonçalves e Thiago Veiga"
 
 if __name__ == '__main__':
     port = os.getenv('PORT')
